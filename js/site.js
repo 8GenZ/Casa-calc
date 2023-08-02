f677222417806ca132d1da1ebbd516dfdb4218f8
8Gen <jonachav6414@gmail.com>
2023-08-02 10:13:42 -0400

    finiished most of the app minor problems left

--- js/site.js
+++ js/site.js
@@ -1,14 +1,91 @@
-function displayMessage() {
-    let inputbox = document.getElementById('message');
+function getValues() {
+    let loan = parseFloat(document.getElementById('loanAmount').value);
+    let term = parseInt(document.getElementById('term').value);
+    let rate = parseFloat(document.getElementById('rate').value);
 
-    let message = inputbox.value;
+    let newLoan = {
+        loan,
+        term,
+        rate,
+    }
+
+    return newLoan;
+
+}
+
+function displayPayments() {
+
+    let loan = getValues();
 
 
-    Swal.fire(
-        {
-            backdrop: false,
-            title: 'App Name',
-            text: message
+    let monthlyPayment = (loan.loan) * (loan.rate / 1200) / (1 - (1 + loan.rate / 1200) ** (-loan.term));
+
+    let totalPrin = loan.loan;
+    let totalCost = monthlyPayment * loan.term;
+    let totalint = totalCost - totalPrin;
+
+    document.getElementById('monthlyPayment').innerText = '$' + monthlyPayment.toFixed(2);
+    document.getElementById('totalPrin').innerText = '$' + totalPrin.toFixed(2);
+    document.getElementById('totalint').innerText = '$' + totalint.toFixed(2);
+    document.getElementById('totalCost').innerText = '$' + totalCost.toFixed(2);
+
+    displayTable(loan);
+
+}
+
+function generateStats(loan) {
+
+    let stats = [];
+
+    for (let month = 1; month <= loan.term; month++) {
+
+        // calculate my numbers here
+        let monthlyPayment = (loan.loan) * (loan.rate / 1200) / (1 - (1 + loan.rate / 1200) ** (-loan.term));
+        let totalCost = monthlyPayment * loan.term;
+        let balance = totalCost - monthlyPayment;
+        let interest = balance * loan.rate / 1200;
+        let principalPayment = monthlyPayment - interest;
+        let totalint = interest + interest;
+
+
+        let payment = {
+            month,
+            monthlyPayment,
+            principal: principalPayment,
+            interest,
+            totalInterest: totalint,
+            balance,
         }
-    )
+
+        stats.push(payment);
+        // push the payment into an array
+    }
+    return stats;
+    // return the array
+}
+
+function displayTable(loan) {
+
+
+    let tableContent = document.getElementById("table-content");
+    let tableTemplate = document.getElementById('table-row-template');
+
+    tableContent.innerHTML = "";
+
+    let payments = generateStats(loan);
+
+    payments.forEach(payment => {
+        // copy template and display on table
+        let tableRow = tableTemplate.content.cloneNode(true);
+
+        tableRow.querySelector('[data-id="month"]').innerText = payment.month;
+        tableRow.querySelector('[data-id="payment"]').innerText = '$' + payment.monthlyPayment.toFixed(2);
+        tableRow.querySelector('[data-id="principal"]').innerText = '$' + payment.principal.toFixed(2);
+        tableRow.querySelector('[data-id="Interest"]').innerText = '$' + payment.interest.toFixed(2);
+        tableRow.querySelector('[data-id="totalint"]').innerText = '$' + payment.totalInterest.toFixed(2);
+        tableRow.querySelector('[data-id="balance"]').innerText = '$' + payment.balance.toFixed(2);
+
+        tableContent.appendChild(tableRow);
+    });
+
 }
